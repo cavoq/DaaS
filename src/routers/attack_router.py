93c3194543680e1a3319a877cdf43a550ff97c31
David Stromberger <DavidStromberger99@gmx.de>
2023-06-12 18:46:37 +0200

    format

--- src/routers/attack_router.py
+++ src/routers/attack_router.py
@@ -14,6 +14,7 @@ async def get_attack_status(attack_id: str, request: Request, api_key_dependency
     status = attack.get_status()
     return Response(status_code=200, content=status, media_type="application/json")
 
+
 @attack_router.get("/stop/{attack_id}")
 async def stop_attack(attack_id: str, request: Request, api_key_dependency: bool = Depends(verify_api_key)):
     attack = attack_controller.get_attack(attack_id)
@@ -21,6 +22,7 @@ async def stop_attack(attack_id: str, request: Request, api_key_dependency: bool
         return Response(status_code=404, content=json.dumps({"status": "Attack not found"}), media_type="application/json")
     attack.stop()
     return Response(status_code=200, content=json.dumps({"status": "Attack stopped"}), media_type="application/json")
+
 
 @attack_router.get("/delete/{attack_id}")
 async def delete_attack(attack_id: str, request: Request, api_key_dependency: bool = Depends(verify_api_key)):
